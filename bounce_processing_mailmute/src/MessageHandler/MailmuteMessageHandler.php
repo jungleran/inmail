5e4663e8e03d7e3846e3507cd8411b2ba9938fb7
Arild Matsson <arild@klavaro.se>
2014-10-22 11:26:17 +0200

    Mailmute handler

---
+++ bounce_processing_mailmute/src/MessageHandler/MailmuteMessageHandler.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\bounce_processing_mailmute\MessageHandler\MailmuteMessageHandler.
+ */
+
+namespace Drupal\bounce_processing_mailmute\MessageHandler;
+
+use Drupal\bounce_processing\DSNType;
+use Drupal\bounce_processing\Message;
+use Drupal\bounce_processing\MessageHandler\MessageHandlerInterface;
+use Drupal\bounce_processing\MessageTypeInterface;
+
+/**
+ * Reacts to bounce messages by managing the send state of the bouncing address.
+ */
+class MailmuteMessageHandler implements MessageHandlerInterface {
+
+  /**
+   * @var \Drupal\bounce_processing\MessageHandler\SendStateManagerInterface
+   */
+  protected $sendStateManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function invoke(Message $message, MessageTypeInterface $type) {
+    if ($type instanceof DSNType && $address = $type->getRecipient()) {
+      // @todo Let Mailmute calculate transitions.
+      if ($user = user_load_by_mail($address)) {
+        $user->field_sendstate = $type->isFailure();
+        $user->save();
+      }
+    }
+  }
+
+}
