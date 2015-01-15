1a5a3ce9884101a26e851d9c5a201aa560136bec
arla <arla@226712.no-reply.drupal.org>
2015-01-15 11:45:44 +0100

    Issue #2405747 by Arla: Test that cron invokes deliverers

---
+++ src/Tests/DelivererTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\inmail\Tests\DelivererTest.
+ */
+
+namespace Drupal\inmail\Tests;
+
+use Drupal\inmail\Entity\DelivererConfig;
+use Drupal\simpletest\KernelTestBase;
+
+/**
+ * Tests mail deliverers.
+ *
+ * @group inmail
+ */
+class DelivererTest extends KernelTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['inmail', 'inmail_test'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Test that Cron runs trigger active deliverers.
+   *
+   * @see inmail_cron()
+   * @see \Drupal\inmail_test\Plugin\inmail\Deliverer\TestDeliverer
+   */
+  public function testCronInvocation() {
+    // Setup deliverer.
+    $deliverer_config = DelivererConfig::create(array(
+      'id' => $this->randomMachineName(),
+      'plugin' => 'test_deliverer',
+    ));
+    $deliverer_config->save();
+
+    // Cron should trigger the deliverer.
+    /** @var \Drupal\Core\CronInterface $cron */
+    $cron = \Drupal::service('cron');
+    $cron->run();
+    $this->assertEqual(\Drupal::state()->get('inmail.test.deliver_count'), 1);
+
+    // Disable deliverer and assert that it is not triggered.
+    $deliverer_config->disable()->save();
+    $cron->run();
+    $this->assertEqual(\Drupal::state()->get('inmail.test.deliver_count'), 1);
+  }
+
+}
---
+++ tests/modules/inmail_test/src/Plugin/inmail/Deliverer/TestDeliverer.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\inmail_test\Plugin\inmail\Deliverer\TestDeliverer.
+ */
+
+namespace Drupal\inmail_test\Plugin\inmail\Deliverer;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\State\StateInterface;
+use Drupal\inmail\Plugin\inmail\Deliverer\DelivererBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Delivers a dummy message and counts invocations.
+ *
+ * @Deliverer(
+ *   id = "test_deliverer",
+ *   label = @Translation("Test")
+ * )
+ */
+class TestDeliverer extends DelivererBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Injected site state.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Constructs a TestDeliverer.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('state'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deliver() {
+    // Increment invocation count.
+    $count = $this->state->get('inmail.test.deliver_count') + 1;
+    $this->state->set('inmail.test.deliver_count', $count);
+
+    // Return one minimal message.
+    return array("Subject: Dummy message $count\n\nFoo");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+  }
+
+}
