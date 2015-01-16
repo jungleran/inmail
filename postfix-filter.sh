c1161601e6f391ecf3fe5a077208ae259c5c4a06
arla <arla@226712.no-reply.drupal.org>
2015-01-16 09:07:11 +0100

    Issue #2403969 by Arla: Allow passing Drush options to Postfix script

--- postfix-filter.sh
+++ postfix-filter.sh
@@ -4,24 +4,33 @@
 #
 # You can setup your local MTA (e.g. Postfix) to pipe incoming email to this
 # script. It will then run the Inmail processor (analyzers and handlers) for
-# each message. Useful for testing the processor on various kinds of email
-# content.
-#
-# You might need to add the location of the drush command to the PATH.
-#
-# As example usage, you can configure Postfix to redirect mail to your mail
-# spool, and put the following in ~/.forward (including quotes):
-# "| PATH=/usr/local/bin:$PATH /var/www/drupal/modules/inmail/postfix-filter.sh"
+# each message.
 
 # Parse options
-while getopts d option; do
+while getopts hdo: option; do
   case $option in
+    h) # Help, usage
+      cat <<EOF
+Pass an email message to 'drush inmail-process'. This is meant to be used as a
+filter for a Postfix alias or similar. See README.txt for an example.
+
+Options:
+  -h                Display this help text and usage information.
+  -d                Print debugging information from the shell environment,
+                    Drush and Inmail.
+  -o <options>      Pass options to Drush.
+EOF
+      exit
+      ;;
     d) # Dump environment information for debugging
       id
       set
       drush status
       drush inmail-services
       DRUSHOPTS=-d
+      ;;
+    o) # Additional Drush options
+      DRUSHOPTS="$DRUSHOPTS $OPTARG"
       ;;
   esac
 done
