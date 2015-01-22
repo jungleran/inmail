5884ce2b34a25650d4a8e4ddceae2a23cc97acc2
Arild Matsson <arild@klavaro.se>
2015-01-22 18:16:00 +0100

    Adapting to updated SchemaInterface

--- inmail_collect/src/Plugin/collect/Schema/InmailMessageSchema.php
+++ inmail_collect/src/Plugin/collect/Schema/InmailMessageSchema.php
@@ -6,13 +6,10 @@
 
 namespace Drupal\inmail_collect\Plugin\collect\Schema;
 
-use Drupal\collect\Entity\Container;
-use Drupal\collect\Plugin\collect\Schema\SchemaInterface;
-use Drupal\Component\Utility\Xss;
+use Drupal\collect\Plugin\collect\Schema\SchemaBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Plugin\PluginBase;
-use Drupal\inmail\MIME\EntityInterface;
-use Drupal\inmail\MIME\MultipartEntity;
+use Drupal\Core\TypedData\ComplexDataInterface;
+use Drupal\Core\TypedData\DataDefinition;
 use Drupal\inmail\MIME\ParseException;
 use Drupal\inmail\MIME\Parser;
 use Drupal\inmail\MIME\Renderer;
@@ -21,9 +18,12 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Schema plugin for Inmail messages.
  *
- * @Schema("https://www.drupal.org/project/inmail/schema/message")
+ * @Schema(
+ *   id = "inmail_message",
+ *   label = @Translation("Email message")
+ * )
  */
-class InmailMessageSchema extends PluginBase implements SchemaInterface, ContainerFactoryPluginInterface {
+class InmailMessageSchema extends SchemaBase implements ContainerFactoryPluginInterface {
 
   /**
    * The injected MIME parser.
@@ -64,77 +64,61 @@ class InmailMessageSchema extends PluginBase implements SchemaInterface, Contain
   /**
    * {@inheritdoc}
    */
-  public function render(Container $container) {
-    $entity = NULL;
+  public function render($data) {
     try {
-      $entity = $this->parseContainer($container);
+      $message = $this->parse($data);
+      return $this->renderer->renderEntity($message);
     }
     catch (ParseException $exception) {
       return array('#markup' => $this->t('Message could not be parsed.'));
     }
-
-    return $this->renderer->renderEntity($entity);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function extractText(Container $container) {
-    try {
-      $entity = $this->parseContainer($container);
-    }
-    catch (ParseException $exception) {
-      return NULL;
-    }
-
-    return $this->renderEntityPlainText($entity);
-  }
-
-  /**
-   * Extracts the MIME Entity from an Inmail-defined Container.
-   *
-   * @param \Drupal\collect\Entity\Container $container
-   *   A Collect Container entity.
-   *
-   * @return \Drupal\inmail\MIME\EntityInterface
-   *   The parsed MIME entity.
-   *
-   * @throws \InvalidArgumentException
-   *   If the schema does not match.
-   * @throws \Drupal\inmail\MIME\ParseException
-   *   If parsing failed.
-   */
-  protected function parseContainer(Container $container) {
-    if ($container->getSchemaUri() != $this->getPluginId()) {
-      throw new \InvalidArgumentException('Container schema does not match plugin.');
-    }
-    $data = json_decode($container->getData());
-    $raw = $data->raw;
+  public function parse($data) {
+    $raw = json_decode($data)->raw;
     return $this->parser->parse($raw);
   }
 
-  protected function renderEntityPlainText(EntityInterface $entity) {
-    if (!$entity instanceof MultipartEntity) {
-      // Add body if it is safe.
-      $body = $entity->getDecodedBody();
-      if (empty($body)) {
-        return NULL;
-      }
-      // @todo This hack prevents "one<br>two" from becoming "onetwo".
-      $body = str_replace('>', '> ', $body);
-      // Strip HTML.
-      return Xss::filter($body, []);
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    $properties['body'] = DataDefinition::create('string')
+      ->setLabel(t('Body'));
+
+    $properties['subject'] = DataDefinition::create('string')
+      ->setLabel(t('Subject'));
+
+    $properties['to'] = DataDefinition::create('string')
+      ->setLabel(t('To'));
+
+    $properties['from'] = DataDefinition::create('string')
+      ->setLabel(t('From'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDataProperty(ComplexDataInterface $data, $property_name) {
+    /** @var \Drupal\inmail\MIME\EntityInterface $message */
+    $message = $data->getValue();
+
+    if ($property_name == 'body') {
+      // @todo Handle MultipartEntity.
+      $value = $message->getDecodedBody();
     }
     else {
-      // Add each contained body part.
-      // @todo Avoid including text of message(s) that this is a reply to.
-      $output = "\n\n";
-      foreach ($entity->getParts() as $part) {
-        $output .= $this->renderEntityPlainText($part) . "\n\n";
-      }
-      // Trim the trailing double newline.
-      return trim($output);
+      $value = $message->getHeader()->getFieldBody($property_name);
     }
+
+    // Using this method creates the typed data as a property of $data.
+    // @todo Inject TypedDataManager.
+    return \Drupal::typedDataManager()->getPropertyInstance($data, $property_name, $value);
   }
 
 }
